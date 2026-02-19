cf37cdb6ec1786baf9d2ae83b660d6e42b9ca80d
ombot-385 <omkar.kumar.dev@gmail.com>
2026-02-19 18:59:42 +0530

    Add files via upload

---
+++ part1/favourite_number.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int favourite_number;
+    cout<<"enter your favourite number";
+    cin>>favourite_number;
+    cout<<"amazing! "<<favourite_number<<" thats my favourite number too"<<endl;
+}
