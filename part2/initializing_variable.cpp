4157992f737c5ab0d23e51dd2458ab4e5b8fed0e
ombot-385 <omkar.kumar.dev@gmail.com>
2026-02-20 19:04:23 +0530

    Add files via upload

---
+++ part2/initializing_variable.cpp
@@ -0,0 +1,19 @@
+// Declaring and initializing variables
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    cout << "Enter the width of the room: ";
+    int room_width {0};
+    cin >> room_width;
+    
+    cout << "Enter the length of the room: ";    
+    int room_length {0};
+    cin >> room_length;
+    
+    cout << "The area of the room is " << room_width * room_length << " square feet" << endl;
+    
+    return 0;
+}
+
