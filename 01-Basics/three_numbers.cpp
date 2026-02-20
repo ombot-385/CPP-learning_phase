a0310be8e2d7038bd2ddd1f754b3c0836689023c
ombot-385 <omkar.kumar.dev@gmail.com>
2026-02-20 19:16:32 +0530

    Add files via upload

---
+++ 01-Basics/three_numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    double c;
+    cout<<"enter an integer"<<endl;
+    cin>>a;
+    cout<<"enter the second integer"<<endl;
+    cin>>b;
+    cout<<"enter a number "<<endl;
+    cin>>c;
+
+    cout<<"the first number is" <<a<<endl;
+    cout<<"the second number is" <<b<<endl;
+    cout<<"the second number is" <<c<<endl;
+}
