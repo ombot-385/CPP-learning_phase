70566f06f518a328ad5f8f61d72b8e5f1f62f5c0
ombot-385 <omkar.kumar.dev@gmail.com>
2026-02-20 19:15:48 +0530

    Delete part1 directory

--- part1/favourite_number.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int favourite_number;
-    cout<<"enter your favourite number";
-    cin>>favourite_number;
-    cout<<"amazing! "<<favourite_number<<" thats my favourite number too"<<endl;
-}
